7485aa2b52fc3d0720098c04a593c3d5c8f36a46
sainaathdev <133358924+sainaathdev@users.noreply.github.com>
2024-08-31 22:45:46 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(cors({
-  origin: 'https://66d348c162742642c8af06e4--sage-treacle-178c00.netlify.app', // replace with your frontend URL
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // if you need to include cookies
-}));
+
 
 mongoose.connect("mongodb://127.0.0.1:27017/test");  //mongoDB compass 
 
